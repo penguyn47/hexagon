e334464bca2d9e4491858c77b177a1971249e799
MrPen <lan474444@gmail.com>
2024-11-05 22:49:43 +0700

    test-1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import vercel from 'vite-plugin-vercel';
 
+// https://vite.dev/config/
 export default defineConfig({
+  css: {
+    sourcemap: false, // Tắt source map
+  },
   server: {
     port: process.env.PORT,
   },
-  plugins: [vercel()],
-});
+  plugins: [react(), vercel()],
+})
