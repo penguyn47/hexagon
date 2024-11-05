0139c5adcbce42a79a9e76abef5bc981914ac307
MrPen <lan474444@gmail.com>
2024-11-05 22:44:32 +0700

    Vercel-test-deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
-// https://vite.dev/config/
 export default defineConfig({
-  css: {
-    sourcemap: false, // Tắt source map
+  server: {
+    port: process.env.PORT,
   },
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-})
+  plugins: [vercel()],
+});
